995a78e1befa8c3699c1d58ff287adc4a1919f34
Sagar Sharma <37947735+sharmaSagar01@users.noreply.github.com>
2023-03-25 20:55:50 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,16 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Can Find you own firebaseConfig File from the firebase Project.
 const firebaseConfig = {
-  apiKey: "AIzaSyCBS9qBY2IdOhQ_44fLsu7b31oQOQVe3m0",
-  authDomain: "netflix-build-clone-54662.firebaseapp.com",
-  projectId: "netflix-build-clone-54662",
-  storageBucket: "netflix-build-clone-54662.appspot.com",
-  messagingSenderId: "1089968179740",
-  appId: "1:1089968179740:web:c55227c10d23b145482dd9",
-  measurementId: "G-ZCQ3V3FFB2",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
